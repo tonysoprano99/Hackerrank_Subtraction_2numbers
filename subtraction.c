36c1a0401f11def70d81af41c450032c630439e4
tonysoprano99 <56022534+tonysoprano99@users.noreply.github.com>
2019-10-01 21:49:53 +0530

    Create subtraction.c

---
+++ subtraction.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int count(int a,int b)
+{
+if(a%b==0)
+{
+return a/b;
+}
+return(a/b+ count(b,a%b));
+}
+int main()
+{
+int a,b,c;
+printf("Enter Values of a and b");
+scanf("%d %d",&a,&b);
+c=count(a,b);
+printf("%d",c);
+return 0;
+}
